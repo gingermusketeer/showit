bda59acff137372d3f6e55a441b1bc3bbc05182f
Max Brosnahan <maximilianbrosnahan@gmail.com>
2014-03-16 21:20:31 +1300

    Modularise

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-var stream = shoe('/slideshow');
 var shoe = require('shoe');
-require('remark/src/remark.js');
-var remark = window.remark;
-var fs = require('fs');
+var getPin = require('./lib/getPin.js');
+var Slideshow = require('./lib/slideshow.js');
+var rawSlides = require('fs').readFileSync('./slides.md').toString(); //Might be a buffer
 
-var rawSlides = fs.readFileSync('./slides.md');
-var config = {
-  container: 'remark-slides',
-  source: rawSlides.toString(),
-};
-
-var slideshow = remark.create(config);
-
-let pin;
-while (!pin || pin.length < 4){
-  pin = prompt('Enter a slideshow pin longer then 4 characters');
-}
-
-var result = document.getElementById('result');
+var takeAction = Slideshow.create(rawSlides, 'slideshow-container');
+var stream = shoe('/slideshow');
 
 stream.on('connect', function(){
+  var pin = getPin();
   stream.write(JSON.stringify({ pin: pin }));
 });
-
-var takeAction = (command) => {
-  switch(command){
-  case 'nextSlide':
-    slideshow.gotoNextSlide();
-    break;
-  case 'previousSlide':
-    slideshow.gotoPreviousSlide();
-    break;
-  default:
-    console.log('Invalid command: ', command);
-  }
-};
 
 stream.on('data', (data) => {
   var object = JSON.parse(data);
